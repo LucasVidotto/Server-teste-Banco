22c39d83d55a7741c20561b5ac3a28c7d92336aa
Lucas Vidotto <lucasvidotto3@gmail.com>
2022-08-17 17:33:06 -0300

    segundo

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ var http = require('http');
 const express = require("express");
 const app = express();
 
+const port = process.env.PORT || 8080;
 app.get('/', (req, res, next) => {
     res.json({ message: "Servidor executado com sucesso!" });
 
 })
 
 var server = http.createServer(app);
-server.listen(8080);
-console.log("Servidor escutando na porta 8080...")
+server.listen(port);
+console.log("Servidor escutando na porta 8080...");
